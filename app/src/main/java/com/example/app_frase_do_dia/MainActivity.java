beb8a0a62f1b8f5a94c7d568ca8a1e4473fece5f
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-09-23 18:56:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/app_frase_do_dia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.app_frase_do_dia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
