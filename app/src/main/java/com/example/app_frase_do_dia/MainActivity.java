12b0cae615e6b787613da03eee9010120bcaa698
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-09-23 19:36:40 -0300

    finalizando projeto

--- app/src/main/java/com/example/app_frase_do_dia/MainActivity.java
+++ app/src/main/java/com/example/app_frase_do_dia/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.app_frase_do_dia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerar_mensagem(View view){
+        String[] frases = {
+                "A alegria de fazer o bem é a única felicidade verdadeira.",
+                "Ser feliz sem motivo é a mais autêntica forma de felicidade.",
+                " Saber encontrar a alegria na alegria dos outros, é o segredo da felicidade.",
+                "A amizade duplica as alegrias e divide as tristezas."
+        };
+        int numero = new Random().nextInt(4);
+        TextView alterar = findViewById(R.id.text_mensagem);
+        alterar.setText( frases[numero]);
+
+
     }
 }
